e065bc0ba7067469d3923af3e70e8176549d9b76
shbhuiyan <shbhuiyan997@gmail.com>
2025-05-04 20:06:58 +0600

    added api key and weather url

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
 import "../Styles/background.css"
+import axios from "axios";
 
 const Home = () => {
+    const [data , setData] = useState({})
+    const [location , setLocation] = useState("")
+
+
+    const apiKey = "c9a72e554d8b43b5d43da2c9f7bafd5c"
+
+    // Weather API req fn
+    const fetchWeather = () => {
+
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`)
+    }
 
     return (
         <section className="background">
@@ -9,6 +22,7 @@ const Home = () => {
                     <input type="text"
                     placeholder="Enter City Name"
                     className="py-3 px-6 w-full border rounded-xl focus:outline-none text-xl"
+                    
                     />
                 </div>
             </div>
