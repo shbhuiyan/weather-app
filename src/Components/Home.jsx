cc8e1029f12e997e3078966ab5958125cc564eab
shbhuiyan <shbhuiyan997@gmail.com>
2025-05-04 19:53:26 +0600

    added background Image

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,21 +1,18 @@
-import React from 'react';
-import { FaSearch } from 'react-icons/fa';
+import "../Styles/background.css"
 
 const Home = () => {
 
-    // const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`
     return (
-        <div className='w-full h-lvh border bg-gradient-to-r from-indigo-500 via-sky-500 to-indigo-500 px-4'>
-            <div className="max-w-[600px] mx-auto my-40">
-                <div className="p-8 rounded-2xl bg-gradient-to-r from-indigo-500 to-sky-700">
-                    <div className="flex">
-                    <input className='py-2 px-6 w-2/3 bg-white rounded-l-full focus:outline-none text-xl' type="text" placeholder='Enter city Name' />
-                    <button className='flex items-center gap-3 py-2 px-6 w-1/3 bg-indigo-400 rounded-r-full text-xl'>Search <FaSearch /></button>
-                    </div>
+        <section className="background">
+            <div className="max-w-[600px] mx-auto px-3 pt-20 lg:pt-36">
+                <div className=" border rounded-2xl shadow-xl backdrop-blur p-8">
+                    <input type="text"
+                    placeholder="Enter City Name"
+                    className="py-3 px-6 w-full border rounded-xl focus:outline-none text-xl"
+                    />
                 </div>
             </div>
-            
-        </div>
+        </section>
     );
 };
 
